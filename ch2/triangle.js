895821cbbda1be74d9b3fd249eda749145c3ddf6
Ramya <ramya@mapbox.com>
2018-01-28 11:22:43 +0530

    ch2 exercise

---
+++ ch2/triangle.js
@@ -0,0 +1,26 @@
+/*
+Write a loop that makes seven calls to console.log to output the following triangle:
+
+#
+##
+###
+####
+#####
+######
+#######
+
+*/
+
+for(i=1;i<8;i++){
+	var j = i;
+	var str ='';
+	
+    do{
+    	str += '#'
+    	
+    	j--;
+
+    }while(j!=0);
+
+	console.log(str)
+}
