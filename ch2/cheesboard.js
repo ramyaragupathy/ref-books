0e198e1f76f3ad6b158d0798b9bb7dfd1c9c2d26
Ramya <ramya@mapbox.com>
2018-01-28 11:19:53 +0530

    chapterwise folders

---
+++ ch2/cheesboard.js
@@ -0,0 +1,53 @@
+
+ /*
+ Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines. 
+ At each position of the grid there is either a space or a “#” character. The characters should form a chess board.
+
+Passing this string to console.log should show something like this:
+
+ # # # #
+# # # #
+ # # # #
+# # # #
+ # # # #
+# # # #
+ # # # #
+# # # #
+When you have a program that generates this pattern, define a variable size = 8 and change the program so that it works 
+for any size, outputting a grid of the given width and height.
+
+*/
+
+function chessboard(size,inputStr){
+	var printStr='',filler=' ';
+	for(i=1;i<=size;i++){
+		if(i%2===0){
+			for(j=0;j<size;j++){
+				if(j%2==0){
+					printStr+=inputStr;
+
+				} else{
+					printStr+=filler;
+
+				}
+				
+			}
+			printStr+='\n';
+		} else {
+			for(j=0;j<size;j++){
+				if(j%2===0){
+					printStr+=filler
+
+				} else{
+					printStr+=inputStr
+
+				}
+				
+			}
+			printStr+='\n';
+		}
+	}
+	return printStr;
+}
+
+console.log(chessboard(8,'#'))
