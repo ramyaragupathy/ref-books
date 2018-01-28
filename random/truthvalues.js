03ba79e7bcba6250460dc725493fb68fde79b894
Ramya <ramya@mapbox.com>
2018-01-28 21:56:34 +0530

    understand truth values

---
+++ random/truthvalues.js
@@ -0,0 +1,20 @@
+var checkZero = 0;
+var checkUndefined;
+var checkNull = null;
+var checkStr = '';
+
+if(!checkZero){
+	console.log('0 is false');
+}
+
+if(!checkUndefined){
+	console.log('undefined is false');
+}
+
+if(!checkNull){
+	console.log('null is false');
+}
+
+if(!checkStr){
+	console.log('"" is false');
+}
