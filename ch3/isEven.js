65d120aaf37dcf722815ed596abc27a50fdfe214
Ramya <ramya@mapbox.com>
2018-01-28 12:03:09 +0530

    statement linewrap + console output

--- ch3/isEven.js
+++ ch3/isEven.js
@@ -1,12 +1,14 @@
-/*We’ve seen that % (the remainder operator) can be used to test whether a number is even or odd by using % 2 to check whether it’s divisible by two. Here’s another way to define whether a positive whole number is even or odd:
+/*We’ve seen that % (the remainder operator) can be used to test whether a number is even or odd by using % 2 to check whether it’s 
+divisible by two. Here’s another way to define whether a positive whole number is even or odd:
 
- Zero is even.
+ - Zero is even.
 
- One is odd.
+ - One is odd.
 
- For any other number N, its evenness is the same as N - 2.
+ - For any other number N, its evenness is the same as N - 2.
 
-Define a recursive function isEven corresponding to this description. The function should accept a number parameter and return a Boolean.
+Define a recursive function isEven corresponding to this description. 
+The function should accept a number parameter and return a Boolean.
 
 Test it on 50 and 75. See how it behaves on -1. Why? Can you think of a way to fix this?*/
 
@@ -23,9 +25,9 @@ function isEven(x){
 }
 
 
-console.log(isEven(-2));
-console.log(isEven(-1));
-console.log(isEven(0));
-console.log(isEven(1))
-console.log(isEven(2));
-console.log(isEven(514));
+console.log(isEven(-2)); //true
+console.log(isEven(-1)); //false
+console.log(isEven(0)); //true
+console.log(isEven(1)); //false
+console.log(isEven(2)); //true
+console.log(isEven(514)); //true
