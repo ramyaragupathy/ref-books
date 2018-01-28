4b179c1f9e674fa498baadab211435c150385744
Ramya <ramya@mapbox.com>
2018-01-28 19:27:35 +0530

    flatten problem statement

--- ch5/flatten.js
+++ ch5/flatten.js
@@ -1,3 +1,7 @@
+/*Use the reduce method in combination with the concat method to
+“flatten” an array of arrays into a single array that has all the elements of the input arrays.
+*/
+
 var arr = [[0,1],[2,3],[4,5],[6,7]];
 var newArr = arr.reduce(function(a,b) {return a.concat(b)},[]);
 
