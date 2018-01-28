f702157e0698dbdd938d67db9bd5036ced8a4160
Ramya <ramya@mapbox.com>
2018-01-28 21:53:06 +0530

    random testing for var names

---
+++ random/variables.js
@@ -0,0 +1,19 @@
+/*
+- The first character must be an ASCII letter (either uppercase or lowercase), or an underscore (_) character. 
+- A number cannot be used as the first character.
+- Subsequent characters must be letters, numbers, or underscores (_).
+- The variable name must not be a reserved word.
+*/
+
+
+//var 123 = 'yes'; //syntaxerror: Unexpected number
+//var he llo ='yes'; //syntaxerror: Unexpected identifier
+//var he/llo ='yes'; //syntaxerror: Unexpected token /
+var he;
+llo ='yes'; //syntaxerror: Unexpected identifier
+//console.log(he;llo); //syntaxerror: missing ) after argument list
+console.log(he); //undefined
+//console.log(she);// Reference error: she is not defined
+console.log(llo); // yes
+//var h&t; //syntaxerror: Unexpected token &
+var _hello;
