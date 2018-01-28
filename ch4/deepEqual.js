fa7cb0a10487b1fdd8e09a3311578b23a3cf06de
Ramya <ramya@mapbox.com>
2018-01-28 17:02:20 +0530

    problem statement for deepequal

--- ch4/deepEqual.js
+++ ch4/deepEqual.js
@@ -1,4 +1,11 @@
+/*Write a function, deepEqual, that takes two values and returns true only if they are the same value or are objects 
+with the same properties whose values are also equal when compared with a recursive call to deepEqual.
 
+To find out whether to compare two things by identity (use the === operator for that) or by looking at their properties,
+you can use the typeof operator. If it produces "object" for both values, you should do a deep comparison. 
+But you have to take one silly exception into account: by a historical accident, typeof null also produces "object".
+
+*/
 function deepEqual(obj1, obj2){
     if(obj1 === null || obj2 === null){
     	return false;
