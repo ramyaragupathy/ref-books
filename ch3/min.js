1a177ca29a5fdfd546b3df1c2aa623bf30a00ac2
Ramya <ramya@mapbox.com>
2018-01-28 11:50:38 +0530

    check values

--- ch3/min.js
+++ ch3/min.js
@@ -1,6 +1,10 @@
 function min(x,y){
-	return Math.min(x,y);
+	if(x<=y){
+		return x;
+	} else return y;
 }
 
 console.log(min(0,-10));
 console.log(min(-15,-10));
+console.log(min(20,30));
+console.log(min(100,100));
