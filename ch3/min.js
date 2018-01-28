c4b288e88b03b15d50456c7804cdfcc2fac7c367
Ramya <ramya@mapbox.com>
2018-01-28 11:48:19 +0530

    Update and rename minimum.js to min.js

---
+++ ch3/min.js
@@ -0,0 +1,6 @@
+function min(x,y){
+	return Math.min(x,y);
+}
+
+console.log(min(0,-10));
+console.log(min(-15,-10));
--- ch3/minimum.js
+++
@@ -1,6 +0,0 @@
-function minimum(x,y){
-	return Math.min(x,y);
-}
-
-console.log(minimum(0,-10));
-console.log(minimum(-15,-10));
