b2c1c8e7c0dd0c62dbc5e9e98b55fbe968ec2246
Ramya Ragupathy <ramyaragupathy@Ramyas-MacBook-Air.local>
2018-02-06 11:00:13 +0530

    typeof as unary

--- random/numbers.js
+++ random/numbers.js
@@ -1,4 +1,12 @@
 console.log(typeof(NaN)); //number
 console.log(typeof(Infinity)); //number
 console.log(typeof(-Infinity)); //number
+
+console.log(typeof NaN); //number
+console.log(typeof Infinity); //number
+console.log(typeof -Infinity); //number
+
+typeof NaN; //doesn't output anything
+typeof(NaN); //doesn't output anything
+
 console.log(-Infinity); //-Infinity
