127012eae110344c0206f324b0ff3a29283e9a8a
Ramya Ragupathy <r.ragupathy85@gmail.com>
2018-02-08 14:44:57 +0530

    ch2 related + operator precedence

---
+++ random/questions/ch2.md
@@ -0,0 +1,20 @@
+1. Keyword to declare values:
+   let, var, const
+
+2. Which of the following keyword are valid?
+   123 - `Invalid`
+   ab,c - `Invalid`
+   _abc - `Valid`
+   $abc - `Valid`
+
+3. What are the values passed to functions called as?
+   arguments
+
+4. What's the output of `console.log(Math.max('r','z'));`?
+   NaN
+
+5. What's the output of `console.log(Math.max(1));`?
+   1
+
+6. What is the direction of control flow in JS?
+   Top to bottom
--- random/truthvalues.js
+++ random/truthvalues.js
@@ -18,3 +18,38 @@ if(!checkNull){
 if(!checkStr){
 	console.log('"" is false');
 }
+
+
+console.log("1" || "2"); //1
+console.log("0" || "2"); //0
+console.log(0 || "2"); //2, because number 0 is false
+console.log("" || "2"); //2, because "" is false
+console.log("1" && "2"); //2
+console.log("0" && "2"); //2
+console.log(0 && "2"); //0, because number 0 is false
+console.log("" && "2"); //"", because "" is false
+console.log(1 && 2); //2
+console.log(2 && (10 * 10 > 50)); //true
+
+
+console.log(!false); //true
+var testBool = !( false );
+console.log(testBool); //true
+testBool = ! ( false && true );
+console.log(testBool) //true
+
+console.log(1+ 1 == 2 && 10 * 10 > 50); //true *,+,>,&&
+console.log(1+ 1 == 5 && 10 * 10 > 50); //false
+console.log(1+ 1 == 1 && 10 * 10 > 50); //false
+console.log(2 && 10 * 10 > 50); //true *,>,&&
+console.log(100 == 1 && 10 * 10 > 50); //false *,>,&&,==
+console.log(100 == 100 * 10 > 50 && 100); //false *,>,&&,==
+
+
+console.log(1 == (100 * 10 > 50) && 100); //100
+console.log(1 == (100 && 10) * 10 > 50); //true operator precedence(),*,>,==
+
+console.log(2 == true); //false
+console.log(true == 2); //false
+
+
