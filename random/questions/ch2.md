49be2b282426b9258a05f2af4cc3b35f3b319abf
Ramya Ragupathy <r.ragupathy85@gmail.com>
2018-02-08 15:08:43 +0530

    additional ch2 questions

--- random/questions/ch2.md
+++ random/questions/ch2.md
@@ -18,3 +18,36 @@
 
 6. What is the direction of control flow in JS?
    Top to bottom
+
+7. What's the output of `Number('abc')`?
+   NaN
+
+8. How do you create a block?
+   Using {}
+
+9. What are words with special meaning called as?
+   Keywords
+
+10. Keyword for alternate execution path using `if` statement
+    else
+
+11. What is the collection of variables/bindings and their values at a given point of time called as?
+    environment
+
+12. Loop control flow can be created using:
+    while, do..while, for
+
+13. Keyword to jump out of the enclosing loop:
+    break
+
+14. Keyword to skip the current iteration of the loop:
+    continue
+
+15. Are comments part of program execution?
+    NO
+
+16. How to write a single line comment?
+    Using //
+
+17. How to write a multiline comment?
+    Using /* ... */
--- random/variables.js
+++ random/variables.js
@@ -1,34 +1,34 @@
-// /*
-// - The first character must be an ASCII letter (either uppercase or lowercase), 
-//or an underscore (_) character. 
-// - A number cannot be used as the first character.
-// - Subsequent characters must be letters, numbers, or underscores (_).
-// - The variable name must not be a reserved word.
-// */
+/*
+- The first character must be an ASCII letter (either uppercase or lowercase), 
+or an underscore (_) character. 
+- A number cannot be used as the first character.
+- Subsequent characters must be letters, numbers, or underscores (_).
+- The variable name must not be a reserved word.
+*/
 
 
-// //var 123 = 'yes'; //syntaxerror: Unexpected number
-// //var he llo ='yes'; //syntaxerror: Unexpected identifier
-// //var he/llo ='yes'; //syntaxerror: Unexpected token /
-// var he;llo ='yes'; 
-// //console.log(he;llo); //syntaxerror: missing ) after argument list
-// console.log(he); //undefined
-// //console.log(she);// Reference error: she is not defined
-// console.log(llo); // yes
-// //var h&t; //syntaxerror: Unexpected token &
-// var _hello;
+//var 123 = 'yes'; //syntaxerror: Unexpected number
+//var he llo ='yes'; //syntaxerror: Unexpected identifier
+//var he/llo ='yes'; //syntaxerror: Unexpected token /
+var he;llo ='yes'; 
+//console.log(he;llo); //syntaxerror: missing ) after argument list
+console.log(he); //undefined
+//console.log(she);// Reference error: she is not defined
+console.log(llo); // yes
+//var h&t; //syntaxerror: Unexpected token &
+var _hello;
 
-// var q= `what
-// gets
-// printed out 
-// of this?`;
-// console.log(q);
+var q= `what
+gets
+printed out 
+of this?`;
+console.log(q);
 
-// var num = 100;
-// var str = `Does this replace 'num' with ${num}?`
-// console.log(str);
+var num = 100;
+var str = `Does this replace 'num' with ${num}?`
+console.log(str);
 
-// console.log('AAAA'<'Z'); //true
+console.log('AAAA'<'Z'); //true
 
 
 
@@ -100,6 +100,12 @@ var yetAnotherNum = Boolean(num);
 console.log(typeof anotherNum); //string
 console.log(yetAnotherNum); //true
 console.log(typeof yetAnotherNum); //boolean
+
+
+console.log(Number('abc'));//NaN
+
+
+
 
 
 
