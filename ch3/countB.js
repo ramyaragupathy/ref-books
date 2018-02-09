6a7c6627aa436d65bfb9a38a88b5c537efecf8d0
Ramya Ragupathy <r.ragupathy85@gmail.com>
2018-02-09 22:41:54 +0530

    standard JS changes

--- ch3/countB.js
+++ ch3/countB.js
@@ -1,15 +1,15 @@
-/*Write a function countBs that takes a string as its only argument and returns a number that indicates 
-how many uppercase “B” characters are in the string.*/
+/* Write a function countBs that takes a string as its only argument and returns a number that indicates
+how many uppercase “B” characters are in the string. */
 
-function countBs(str){
-	var length = str.length;
-	var count = 0;
-	for(i=0;i<length;i++){
-		if(str[i] === 'B'){
-			count++;
-		}
-	}
-	return count;
+const countBs = (str) => {
+  let length = str.length
+  let count = 0
+  for (let i = 0; i < length; i++) {
+    if (str[i] === 'B') {
+      count++
+    }
+  }
+  return count
 }
 
-console.log(countBs("BBC")); //2
+console.log(countBs('BBC')) // 2
