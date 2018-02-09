fdbee7f1c08d63184de2b32f5b0909596fddaec2
Ramya Ragupathy <r.ragupathy85@gmail.com>
2018-02-09 22:58:03 +0530

    standard JS + => functions

--- ch4/deepEqual.js
+++ ch4/deepEqual.js
@@ -1,52 +1,45 @@
-/*Write a function, deepEqual, that takes two values and returns true only if they are the same value or are objects 
+/* Write a function, deepEqual, that takes two values and returns true only if they are the same value or are objects
 with the same properties whose values are also equal when compared with a recursive call to deepEqual.
 
 To find out whether to compare two things by identity (use the === operator for that) or by looking at their properties,
-you can use the typeof operator. If it produces "object" for both values, you should do a deep comparison. 
+you can use the typeof operator. If it produces "object" for both values, you should do a deep comparison.
 But you have to take one silly exception into account: by a historical accident, typeof null also produces "object".
 
 */
-function deepEqual(obj1, obj2){
-    if(obj1 === null || obj2 === null){
-    	return false;
-    } else if (obj1 === obj2){
-		//console.log('same to same');
-		return true;
-	}  else {
-		//console.log(Object.keys(obj1).length)
-		//console.log(Object.keys(obj2).length)
-		if(Object.keys(obj1).length === Object.keys(obj2).length){
-
-			//console.log('same number of properties')
-        	for(var property in obj1){
-        		//console.log(obj1[property]);
-        		//console.log(obj2[property]);
-        	 	if(!(property in obj2) || !deepEqual(obj1[property], obj2[property])){
-        	 		return false;
-        	 	} else{
-        	 		return true;
-        	 	}
-      			
-       	 }
-    	} else{
-    		return false;
-    	}
-
-	}
-
-	
-
+const deepEqual = (obj1, obj2) => {
+  if (obj1 === null || obj2 === null) {
+    return false
+  } else if (obj1 === obj2) {
+  // console.log('same to same');
+    return true
+  } else {
+  // console.log(Object.keys(obj1).length)
+  // console.log(Object.keys(obj2).length)
+    if (Object.keys(obj1).length === Object.keys(obj2).length) {
+      // console.log('same number of properties')
+      for (var property in obj1) {
+        // console.log(obj1[property]);
+        // console.log(obj2[property]);
+        if (!(property in obj2) || !deepEqual(obj1[property], obj2[property])) {
+          return false
+        } else {
+          return true
+        }
+      }
+    } else {
+      return false
+    }
+  }
 }
 
-
-var obj = {here: {is: "an"}, object: 2};
-console.log(deepEqual(obj,obj));
-console.log(deepEqual(null,null));
-console.log(deepEqual(null,obj));
-console.log(deepEqual(obj,null));
-console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
-console.log(deepEqual(obj, {here: 1, object: 2}));
-console.log(deepEqual({'name':1}, {'name':123}));
-console.log(deepEqual({'name':1}, {'name':1}));
-console.log(deepEqual({'name':'ramya'}, {'name':123}));
-console.log(deepEqual({'name':'ar'}, {'name':{123:123,212:212}}));
+var obj = {here: {is: 'an'}, object: 2}
+console.log(deepEqual(obj, obj))
+console.log(deepEqual(null, null))
+console.log(deepEqual(null, obj))
+console.log(deepEqual(obj, null))
+console.log(deepEqual(obj, {here: {is: 'an'}, object: 2}))
+console.log(deepEqual(obj, {here: 1, object: 2}))
+console.log(deepEqual({'name': 1}, {'name': 123}))
+console.log(deepEqual({'name': 1}, {'name': 1}))
+console.log(deepEqual({'name': 'ramya'}, {'name': 123}))
+console.log(deepEqual({'name': 'ar'}, {'name': {123: 123, 212: 212}}))
--- ch4/reverseArray.js
+++ ch4/reverseArray.js
@@ -1,33 +1,32 @@
-/*Arrays have a method reverse, which changes the array by inverting the order in which its elements appear. 
-For this exercise, write two functions, reverseArray and reverseArrayInPlace. 
-The first, reverseArray, takes an array as argument and produces a new array that has the same elements in the inverse order. 
-The second, reverseArrayInPlace, does what the reverse method does: it modifies the array given as argument in order to reverse its 
-elements. 
-Neither may use the standard reverse method. 
+/* Arrays have a method reverse, which changes the array by inverting the order in which its elements appear.
+For this exercise, write two functions, reverseArray and reverseArrayInPlace.
+The first, reverseArray, takes an array as argument and produces a new array that has the same elements in the inverse order.
+The second, reverseArrayInPlace, does what the reverse method does: it modifies the array given as argument in order to reverse its
+elements.
+Neither may use the standard reverse method.
 */
 
-function reverseArray(arr){
-	var newArr = [];
-	var length = arr.length;
-	for(i=length-1;i>=0;i--){
-		newArr.push(arr[i]);
-	}
-	return newArr;
+const reverseArray = (arr) => {
+  let newArr = []
+  let length = arr.length
+  for (let i = length - 1; i >= 0; i--) {
+    newArr.push(arr[i])
+  }
+  return newArr
 }
 
-function reverseArrayInPlace(arr){
-	var length = arr.length;
-	
-	for(i=0;i<length/2;i++){
-		var temp = arr[i];
-		arr[i] = arr[length-1 -i];
-		arr[length-1-i] = temp;
-		
-	}
-	return arr;
+const reverseArrayInPlace = (arr) => {
+  var length = arr.length
+
+  for (let i = 0; i < length / 2; i++) {
+    var temp = arr[i]
+    arr[i] = arr[length - 1 - i]
+    arr[length - 1 - i] = temp
+  }
+  return arr
 }
 
-console.log(reverseArray(["A", "B", "C"])); //  ["C", "B", "A"];
-var arrayValue = [1, 2, 3, 4, 5];
-reverseArrayInPlace(arrayValue);
-console.log(arrayValue); //[5, 4, 3, 2, 1]
+console.log(reverseArray(['A', 'B', 'C'])) //  ["C", "B", "A"];
+var arrayValue = [1, 2, 3, 4, 5]
+reverseArrayInPlace(arrayValue)
+console.log(arrayValue) // [5, 4, 3, 2, 1]
