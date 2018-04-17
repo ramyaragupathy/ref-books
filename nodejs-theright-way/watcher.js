a99586012cd44430489cdce4c6d24873c2734685
Ramya Ragupathy <r.ragupathy85@gmail.com>
2018-04-17 12:26:27 +0530

    nodejs ref

---
+++ nodejs-theright-way/watcher.js
@@ -0,0 +1,13 @@
+const fs = require('fs')
+const spawn = require('child-process').spawn
+const filename = process.argv[2]
+
+if (!filename) {
+  throw Error('A file to watch must be specified')
+}
+fs.watch('target.txt', function () {
+  let ls = spawn('ls', ['-lh', filename])
+  ls.stdout.pipe(process.stdout)
+  console.log('File ' + filename + ' just changed')
+})
+console.log('Now watching ' + filename + ' for changes...')
