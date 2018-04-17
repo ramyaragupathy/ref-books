239d78814109ec6ad7992e9c5be7850adf005cb9
Ramya Ragupathy <r.ragupathy85@gmail.com>
2018-04-17 13:01:21 +0530

    child process

--- nodejs-theright-way/watcher.js
+++ nodejs-theright-way/watcher.js
@@ -1,5 +1,6 @@
+'use strict'
 const fs = require('fs')
-const spawn = require('child-process').spawn
+const spawn = require('child_process').spawn
 const filename = process.argv[2]
 
 if (!filename) {
@@ -8,6 +9,6 @@ if (!filename) {
 fs.watch('target.txt', function () {
   let ls = spawn('ls', ['-lh', filename])
   ls.stdout.pipe(process.stdout)
-  console.log('File ' + filename + ' just changed')
+  // console.log('File ' + filename + ' just changed')
 })
 console.log('Now watching ' + filename + ' for changes...')
