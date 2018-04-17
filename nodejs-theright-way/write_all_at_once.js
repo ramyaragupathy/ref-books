5d4666272ae8def58d95291d94f893caa952dd2c
Ramya Ragupathy <r.ragupathy85@gmail.com>
2018-04-17 15:37:14 +0530

    fortify code

--- nodejs-theright-way/write_all_at_once.js
+++ nodejs-theright-way/write_all_at_once.js
@@ -1,6 +1,8 @@
+#!/usr/bin/env node
 const fs = require('fs')
 
-// Overwrites existing content
+// Creates file if it doesn't exist
+// If file exists, overwrites existing content
 fs.writeFile('target.txt', 'my message', function (err) {
   if (err) throw err
   else console.log('File saved!')
